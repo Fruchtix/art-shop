8932477df7f497ff03f143956e86ff9429dbce24
unknown <jan@fuechtener.eu>
2021-01-01 20:09:33 +0100

    changed styling from jumbotron

--- src/components/ctaBox/index.js
+++ src/components/ctaBox/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import styles from "./ctaBox.module.css";
-
+import { Link } from "react-router-dom";
 function CtaBox() {
   return (
     <div className={styles.container}>
-      <h1>CtaBox</h1>
+      <div className={styles.maxWidth}>
+        <div className={styles.textWrapper}>
+          <h1 className={styles.headline}>Kunst für dich</h1>
+          <h3 className={styles.subHeadline}>
+            Hol dir jetzt handgemachte Kunst für dein Zuhause und unterstütze
+            lokale Künstler.
+          </h3>
+        </div>
+        <div className={styles.btnWrapper}>
+          <Link to="/art">
+            <button className={styles.ctaBtn}>Werke ansehen</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
